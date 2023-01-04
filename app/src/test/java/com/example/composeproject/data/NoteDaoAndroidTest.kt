3144646dd806944e4718f5f0b68e3ca7632e84cb
Guillaume-FAURE <faureg0717@gmail.com>
2023-01-04 13:57:03 +0100

    Title Of Commit
    Test of a new commiting method with editor, title and body

--- app/src/test/java/com/example/composeproject/data/NoteDaoAndroidTest.kt
+++ app/src/test/java/com/example/composeproject/data/NoteDaoAndroidTest.kt
@@ -1,4 +1,13 @@
 package com.example.composeproject.data
 
+import androidx.room.Room
+import androidx.test.platform.app.InstrumentationRegistry
+
 class NoteDaoAndroidTest {
+
+    private lateinit var systemUnderTest: ArtDAO
+    private var memoryDatabase = Room.inMemoryDatabaseBuilder(
+        InstrumentationRegistry.getInstrumentation().context,
+        AppDatabase::class.java
+    ).allowMainThreadQueries().build()
 }
